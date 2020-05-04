9ff14c08b05bd43f5baae3db92785a7d0ae83daf
Tush37 <39132762+Tush37@users.noreply.github.com>
2020-05-05 00:56:41 +0530

    Update BuildScript.cs

--- TestProject/Assets/Scripts/Editor/BuildScript.cs
+++ TestProject/Assets/Scripts/Editor/BuildScript.cs
@@ -2,8 +2,24 @@ using UnityEditor;
 
 public class BuildScript 
 {
-    [MenuItem("Custom Utilities/Build WebGL")]
+     [MenuItem("Custom Utilities/Build StandaloneLinux64")]
     static void PerformBuild()
+    {
+        string[] defaultScene = { "Assets/Scenes/MainScene.unity" };
+        BuildPipeline.BuildPlayer(defaultScene, "./builds/game.x86_64",
+            BuildTarget.StandaloneLinux64, BuildOptions.None);
+    }
+
+    [MenuItem("Custom Utilities/Build Asset Bundle StandaloneLinux64")]
+    static void PerformAssetBundleBuild()
+    {
+        BuildPipeline.BuildAssetBundles("../AssetBundles/", BuildAssetBundleOptions.ChunkBasedCompression,
+            BuildTarget.StandaloneLinux64);
+    }
+    
+    
+    [MenuItem("Custom Utilities/Build WebGL")]
+    static void PerformBuildWebgl()
     {
         string[] defaultScene = { "Assets/Scenes/MainScene.unity" }; 
         string pathToDeploy = "builds/WebGLversion/";       
@@ -11,5 +27,6 @@ public class BuildScript
         BuildPipeline.BuildPlayer(defaultScene, pathToDeploy, BuildTarget.WebGL, BuildOptions.None);
         
     }
+    
 
 }
